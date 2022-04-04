3c243a68e96dd8b33ea5d84b0a90fe92aaccb04d
Amaury <amaury.lopezdiaz@gmail.com>
2022-04-04 16:57:22 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const ctrl = require('./controller')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static('../public'))
 app.get('/',function(req,res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
   });
 
 app.get('/soaps', ctrl.getSoaps)
