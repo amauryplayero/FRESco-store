b512dc0af2e22dce602bfd3cde86f89894acc9aa
Amaury <amaury.lopezdiaz@gmail.com>
2022-04-04 16:44:02 -0400

    test

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -15,7 +15,7 @@ const [shirts, setShirts] = useState([])
 
 
 const getSoaps = () =>{
-    axios.get(`/soaps/`).then(res =>{
+    axios.get(`soaps`).then(res =>{
         let soaps = []
         let product = res.data
         // console.log(product)
@@ -27,7 +27,7 @@ const getSoaps = () =>{
 }
     
     const getCds = () => {
-        axios.get(`/cds/`).then(res =>{
+        axios.get(`cds`).then(res =>{
             let cds = []
             let product = res.data
             // console.log(product)
