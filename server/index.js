686e5104620058a3f77868e7dd31b828cd4027fc
Amaury <amaury.lopezdiaz@gmail.com>
2022-04-05 15:26:48 -0400

    test heroku

--- server/index.js
+++ server/index.js
@@ -10,10 +10,11 @@ const ctrl = require('./controller')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('../public'))
-app.get('/',function(req,res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
-  });
+app.use(express.static(path.resolve(__dirname, '../public')))
+
+app.get('/*', function (req,res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+})
 
 app.get('/soaps', ctrl.getSoaps)
 app.get("/cds", ctrl.getCds)
